7a3eff8fbb0799f7936bca5aab67c987b2e5aa5b
Matous Kucera <mk@kcra.me>
2022-09-17 09:38:30 +0200

    refactor & deps: rework console, update noVNC

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/google/uuid v1.3.0
 	github.com/lusory/libkitsune v0.0.0-20220902174542-3e64336ec215
+	github.com/peterh/liner v1.2.2
 	github.com/rodaine/table v1.0.1
 	github.com/urfave/cli/v2 v2.11.2
 	google.golang.org/grpc v1.49.0
@@ -19,10 +20,12 @@ require (
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
-	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+	golang.org/x/sys v0.0.0-20220915200043-7b5979e65e41 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220902135211-223410557253 // indirect
 )
--- handler/console.go
+++ handler/console.go
@@ -1,45 +1,101 @@
 package handler
 
 import (
-	"bufio"
 	"context"
-	"fmt"
+	"errors"
 	"github.com/fatih/color"
+	"github.com/peterh/liner"
 	"github.com/urfave/cli/v2"
 	"os"
+	"path/filepath"
 	"strings"
 )
+
+// RecursiveConsoleError is an error which gets raised when a user attempts to invoke the console command in a console.
+var RecursiveConsoleError = errors.New("the console command cannot be invoked inside of itself")
 
 // ConsoleCtxKey is a context key for commands invoked in an interactive console.
 var ConsoleCtxKey = "console command"
 
+// historyFile is the path to the command history file of the interactive console.
+var historyFile string
+
+func init() {
+	// save the history file into the home dir or the current working directory
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		homeDir, _ = os.Executable()
+	}
+
+	historyFile = filepath.Join(homeDir, ".kitsh_history")
+}
+
 // Console is a handler for the "console" command.
 func Console(cCtx *cli.Context) error {
-	reader := bufio.NewReader(os.Stdin)
+	if cCtx.Context.Value(ConsoleCtxKey) != nil {
+		return RecursiveConsoleError
+	}
+
 	file, _ := os.Executable()
 
-	for {
-		fmt.Print("kitsh> ")
-		text, _ := reader.ReadString('\n')
-		args := splitBySpace(strings.TrimSuffix(text, "\n"))
-		for i := range args {
-			// remove leading and trailing quotes
-			args[i] = strings.TrimFunc(args[i], func(r rune) bool {
-				return r == '\'' || r == '"'
-			})
-		}
-		for len(args) > 0 && strings.HasPrefix(args[0], "-") {
-			args = args[1:] // remove any global arguments
-		}
-		if len(args) == 0 {
-			continue
-		}
+	line := liner.NewLiner()
+	defer line.Close()
 
-		newArgs := append([]string{file, "--target", cCtx.String("target")}, args...)
-		if err := cCtx.App.RunContext(context.WithValue(context.Background(), ConsoleCtxKey, args), newArgs); err != nil {
-			color.Red("%s", err)
+	line.SetCtrlCAborts(true)
+	// only completes the base command names for the time being
+	line.SetCompleter(func(line string) (c []string) {
+		for _, n := range cCtx.App.VisibleCommands() {
+			if strings.HasPrefix(n.Name, strings.ToLower(line)) {
+				c = append(c, n.Name)
+			}
+		}
+		return
+	})
+
+	if f, err := os.Open(historyFile); err == nil {
+		line.ReadHistory(f)
+		f.Close()
+	}
+
+	defer func() {
+		if f, err := os.Create(historyFile); err == nil {
+			line.WriteHistory(f)
+			f.Close()
+		} else {
+			color.Red("Error writing history file: %s", err)
+		}
+	}()
+
+	for {
+		if text, err := line.Prompt("kitsh> "); err == nil {
+			args := splitBySpace(strings.TrimSuffix(text, "\n"))
+			for i := range args {
+				// remove leading and trailing quotes
+				args[i] = strings.TrimFunc(args[i], func(r rune) bool {
+					return r == '\'' || r == '"'
+				})
+			}
+			for len(args) > 0 && strings.HasPrefix(args[0], "-") {
+				args = args[1:] // remove any global arguments
+			}
+			if len(args) == 0 {
+				continue
+			}
+
+			newArgs := append([]string{file, "--target", cCtx.String("target")}, args...)
+			if err := cCtx.App.RunContext(context.WithValue(context.Background(), ConsoleCtxKey, args), newArgs); err != nil {
+				color.Red("%s", err)
+			}
+
+			line.AppendHistory(text)
+		} else if err == liner.ErrPromptAborted { // Ctrl+C
+			break
+		} else {
+			color.Red("Failed to read input: %s", err)
 		}
 	}
+
+	return nil
 }
 
 // splitBySpace splits the supplied string on spaces, honoring double and single-quoted substrings.
